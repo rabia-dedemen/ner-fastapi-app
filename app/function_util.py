91c71e8b895a280dd7da99532374f7d528042cb9
rabia-dedemen <rd@ogr.eskisehir.edu.tr>
2024-05-20 19:45:40 +0300

    initial commit

---
+++ app/function_util.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c21a8086772f7b1b31192407e13d8cb1458b691fbbee3ce16a7801a8d6fd3b24
+size 1121
---
+++ app/main.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ab3365b6979ac55f4bf616b0c6a30507be0650b348e4722a31f93681f0229603
+size 2120
