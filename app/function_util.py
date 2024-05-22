2c9763a9e207fff739adf9919e0188db73c97e99
rabia-dedemen <154626676+rabia-dedemen@users.noreply.github.com>
2024-05-22 17:28:20 +0300

    Update function_util.py

--- app/function_util.py
+++ app/function_util.py
@@ -1,10 +1,9 @@
-import spacy
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
 import gensim
-import nltk
 
-nlp = spacy.load("en_core_web_sm")
 stop_words = set(stopwords.words('english'))
 lemmatizer = WordNetLemmatizer()
 
@@ -24,11 +23,10 @@ def preprocess(text):
     stop_words = stopwords.words('english')
     wn = WordNetLemmatizer()
 
-    doc = nlp(text)
-    filtered_tokens = [token.text for token in doc if token.ent_type_ not in ['PERSON', 'GPE']]
+    filtered_tokens = [word for word, pos in pos_tag(word_tokenize(text)) if pos not in ['PERSON', 'GPE']]
 
     tokens = gensim.utils.simple_preprocess(' '.join(filtered_tokens), deacc=True)
-    return [wn.lemmatize(tok, simplify(pos)) for tok, pos in nltk.pos_tag(tokens) if tok not in stop_words]
+    return [wn.lemmatize(tok, simplify(pos)) for tok, pos in pos_tag(tokens) if tok not in stop_words]
 
 
 def get_document_topics(model, bow):
